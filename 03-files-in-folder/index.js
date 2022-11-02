3e80327c570f1638f86d9091045a8b9ff4012300
Radionov_Vitaly <radionovv89@gmail.com>
2022-11-02 22:22:49 +0300

    feat: add solution, task 03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, './secret-folder'), { withFileTypes: true }, (error, fileContent) => {
+  if (error) throw error;
+  let arrFiles = fileContent.filter(element => element.isFile());
+  console.log(arrFiles);
+  arrFiles.forEach((element) => {
+    fs.stat(path.join(__dirname, './secret-folder', element.name), (error, fileContent) => {
+      if (error) throw error;
+      console.log(`${path.parse(element.name).name} - ${path.extname(element.name)} - ${fileContent.size} (${Math.ceil(fileContent.size / 1024)} KB)`);
+    });
+  });
+});
