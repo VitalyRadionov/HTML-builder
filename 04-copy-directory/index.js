7bc605eda5d3a1c071fb33aa68d2e45cb157016e
Radionov_Vitaly <radionovv89@gmail.com>
2022-11-03 21:38:34 +0300

    feat: add solution, task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const routToFiles = path.join(__dirname, '/files');
+const routToFilesCopy = path.join(__dirname, '/files-copy');
 
+copyDir();
+
+function copyDir() {
+  fs.readdir(routToFilesCopy, { withFileTypes: true }, (error, filesCopyContent) => {
+    if (error) {
+      console.log("\x1b[33m", `directory 'files-copy' does not exist`, '\x1b[0m');
+      console.log(`make directory 'files-copy'...`);
+      fs.mkdir(routToFilesCopy, { recursive: true }, (err) => {
+        if (err) throw err;
+        else copyDir();
+      });
+    }
+    else {
+      console.log(`directory 'files-copy' exists`);
+      console.log('copying...');
+
+      fs.readdir(routToFiles, { withFileTypes: true }, (err, fileContent) => {
+        if (err) throw err;
+        else {
+          let arrFiles = fileContent.filter(element => element.isFile());
+          // console.log(arrFiles);
+          let arrFilesCopy = filesCopyContent.filter(element => arrFiles.find(item => item.name == element.name) == undefined);
+          // console.log(arrFilesCopy);
+          arrFiles.forEach((element) => {
+            fs.copyFile(path.join(routToFiles, element.name), path.join(routToFilesCopy, element.name), (err) => {
+              if (err) throw err;
+              else { console.log(`${element.name} was copied`); }
+            });
+          });
+          if (arrFilesCopy.length) {
+            // console.log(arrFilesCopy);
+            arrFilesCopy.forEach(element => {
+              fs.unlink(path.join(routToFilesCopy, element.name), (err) => {
+                if (err) throw err;
+                else { console.log('\x1b[31m', `${element.name} was deleted`, '\x1b[0m'); }
+              });
+            })
+          }
+        }
+      });
+    }
+  });
+}
