4d40fb542c2289f70434e3c0ff21e83e4813973f
Sam Gold <Radionovv89@gmail.com>
2023-05-03 16:28:00 +0300

    feat: delete method

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 fs.readdir(path.join(__dirname, './secret-folder'), { withFileTypes: true }, (error, fileContent) => {
   if (error) throw error;
-  let arrFiles = fileContent.filter(element => element.isFile());
-
-  arrFiles.forEach((element) => {
-    fs.stat(path.join(__dirname, './secret-folder', element.name), (error, fileContent) => {
-      if (error) throw error;
-      console.log(`${path.parse(element.name).name} - ${path.extname(element.name)} - ${fileContent.size} (${Math.ceil(fileContent.size / 1024)} KB)`);
-    });
+  fileContent.forEach(element => {
+    if (element.isFile()) {
+      fs.stat(path.join(__dirname, './secret-folder', element.name), (error, fileContent) => {
+        if (error) throw error;
+        console.log(`${path.parse(element.name).name} - ${path.extname(element.name)} - ${fileContent.size} (${(fileContent.size / 1024).toFixed(2)} KB)`);
+      });
+    }
   });
 });
