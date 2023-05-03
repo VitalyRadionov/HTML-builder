fc614b24beca5fcd5209189faf811d4a4b703815
Sam Gold <Radionovv89@gmail.com>
2023-05-03 15:45:41 +0300

    feat: delete commented code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,14 +1,5 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const rr = fs.createReadStream(path.join(__dirname, './text.txt'), 'utf-8');
+const rs = fs.createReadStream(path.join(__dirname, './text.txt'), 'utf-8');
 
-// Event: 'data'
-rr.on('data', (data) => {
-  console.log(data);
-});
-
-// fs.readFile
-// fs.readFile(path.join(__dirname, './text.txt'), 'utf-8', (error, fileContent) => {
-//   if (error) throw error;
-//   console.log(fileContent);
-// });
+rs.on('data', (data) => console.log(data));
