ce50bf16bdaae6a215d1a159852e6f007cb03068
Sam Gold <Radionovv89@gmail.com>
2023-05-03 17:06:33 +0300

    refactor: delete function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,26 +3,22 @@ const path = require('path');
 const routToFiles = path.join(__dirname, '/styles');
 const bundle = fs.createWriteStream(path.join(__dirname, '/project-dist', 'bundle.css'));
 
-mergeStylesCSS();
+fs.readdir(routToFiles, { withFileTypes: true }, (err, filesCSS) => {
+  if (err) {
+    console.log("\x1b[33m", `directory 'styles' does not exist`, '\x1b[0m');
+  }
+  else {
+    console.log('build...');
+    let arrFilesCSS = filesCSS.filter(element => path.extname(element.name) == '.css');
+    console.log(arrFilesCSS);
 
-function mergeStylesCSS() {
-  fs.readdir(routToFiles, { withFileTypes: true }, (err, filesCSS) => {
-    if (err) {
-      console.log("\x1b[33m", `directory 'styles' does not exist`, '\x1b[0m');
-    }
-    else {
-      console.log('build...');
-      let arrFilesCSS = filesCSS.filter(element => path.extname(element.name) == '.css');
-      console.log(arrFilesCSS);
-
-      arrFilesCSS.forEach(element => {
-        fs.readFile(path.join(routToFiles, element.name), "utf8", (err, fileContent) => {
-          if (err) throw err;
-          else {
-            bundle.write(`${fileContent}\n`);
-          }
-        });
+    arrFilesCSS.forEach(element => {
+      fs.readFile(path.join(routToFiles, element.name), "utf8", (err, fileContent) => {
+        if (err) throw err;
+        else {
+          bundle.write(`${fileContent}\n`);
+        }
       });
-    }
-  });
-}
+    });
+  }
+});
